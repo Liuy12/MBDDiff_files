12f85c0ed4b5747b918ecd5cbcc9e65448122b9f
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2016-04-20 10:37:18 -0500

    add README and update script

--- script.R
+++ script.R
@@ -7,90 +7,36 @@ library(pryr)
 library(doMC)
 
 registerDoMC(40)
+
+### Generate background annotation file for mm9
 Promoter_anno <- GetPromoterAnno('mm9', save = T, Dir = './')
 t1 <- Sys.time()
 bed_100bp_bg <- IdentifyBackground(organism = 'mm9', bed_path = './', binsize = 100, promo_bed = Promoter_anno, fa ='/illumina/iGenomes/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/genome.fa')
 t2 <- Sys.time()
 t2 - t1
+write.table(bed_100bp_bg, 'mm9Bg.bed', sep = '\t', col.names = F, quote =F)
 
-
-
-
-
-#Promoter_anno <- GetPromoterAnno('hg18', save = T, Dir = './')
-Promoter_anno <- fread('Promoter_Anno.bed', data.table= F)
-#bed_100bp_bg <- IdentifyBackground(organism = 'hg19', bed_path = './', binsize = 100, promo_bed = Promoter_anno, fa ='/illumina/iGenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa' , cores = 20)
-
-
-# cl <- makeCluster(40, outfile = "Process.txt")
-# registerDoParallel(cl)
-registerDoMC(40)
-
-chromlen <- as.data.frame(GetChromLength('hg18'))
-chromlen <- chromlen[which(sapply(strsplit(chromlen$chrom, "_"), length) ==1),]
-bed_100bp <- as.data.frame(CreateWindows(chromlen, 100))
-rm(chromlen)
-bed_path <- paste('./', 'bed_100bp.bed', sep = '')
-storage.mode(bed_100bp[,2]) <-
-  storage.mode(bed_100bp[,3]) <- 'integer'
-cat("Write bed file to disk", "\t")
-write.table(
-  bed_100bp, bed_path, quote = FALSE, sep = '\t', row.names = FALSE, col.names = FALSE
-)
-Exclude_regions <-
-  as.data.frame(ConstructExRegions('hg18', Promoter_anno))
-Getfasta('/illumina/iGenomes/Homo_sapiens/UCSC/hg18/Sequence/WholeGenomeFasta/genome.fa', bed_path)
-Exclude_index <- ExcludeIntersection(bed_100bp, Exclude_regions)
-rm(Exclude_regions)
-letter_freq <- CountFreqency('./bed_100bp.fa')
-Exclude_index1 <- which(letter_freq$ATCG != 1)
-Exclude_index_all <-
-  Reduce(dplyr::union, list(Exclude_index, Exclude_index1))
-rm(list = c("Exclude_index", "Exclude_index1"))
-bed_100bp_filtered <- 
-  bind_cols(bed_100bp[-Exclude_index_all,], letter_freq[-Exclude_index_all,][1])
-rm(list = c("Exclude_index_all", "bed_100bp", "letter_freq"))
-colnames(bed_100bp_filtered) <-
-  c('chrom', 'chromStart', 'chromEnd', 'GC')
-
+### Generate background annotation file for mm10
+Promoter_anno <- GetPromoterAnno('mm10', save = T, Dir = './')
 t1 <- Sys.time()
-Background_region <-
-  foreach (
-    i = 1:nrow(Promoter_anno), .options.multicore = list(preschedule = FALSE),
-    .combine = bind_rows, .inorder = TRUE, .verbose =
-      TRUE,
-    .errorhandling = 'stop', .multicombine =
-      TRUE
-  ) %dopar% {
-    cat('promoter', i, '\n')
-    preset <-
-      filter(bed_100bp_filtered, chrom == Promoter_anno$chrom[i])
-    preset <-
-      mutate(
-        preset, proximity = abs(chromStart - Promoter_anno$chromStart[i]),
-        geneName = Promoter_anno$name[i]
-      )
-    preset <-
-      arrange(preset, proximity)
-    k <- 0
-    bgregion <- c()
-    for (j in 1:nrow(preset)) {
-      if (preset[j,4] < 0.4) {
-        k <- k + 1
-        bgregion <-
-          bind_rows(bgregion, preset[j,])
-      }
-      if (k == 80)
-        break
-    }
-    bgregion
-  }
+bed_100bp_bg <- IdentifyBackground(organism = 'mm10', bed_path = './', binsize = 100, promo_bed = Promoter_anno, fa ='/illumina/iGenomes/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa')
 t2 <- Sys.time()
 t2 - t1
+write.table(bed_100bp_bg, 'mm10Bg.bed', sep = '\t', col.names = F, quote =F)
 
+### Generate background annotation file for hg18
+Promoter_anno <- GetPromoterAnno('hg18', save = T, Dir = './')
+t1 <- Sys.time()
+bed_100bp_bg <- IdentifyBackground(organism = 'hg18', bed_path = './', binsize = 100, promo_bed = Promoter_anno, fa ='/illumina/iGenomes/Homo_sapiens/UCSC/hg18/Sequence/WholeGenomeFasta/genome.fa')
+t2 <- Sys.time()
+t2 - t1
+write.table(bed_100bp_bg, 'hg1Bg.bed', sep = '\t', col.names = F, quote =F)
 
-rm(list = c("bed_100bp_filtered", "Promoter_anno"))
-
-
-
+### Generate background annotation file for hg19
+Promoter_anno <- GetPromoterAnno('hg19', save = T, Dir = './')
+t1 <- Sys.time()
+bed_100bp_bg <- IdentifyBackground(organism = 'hg19', bed_path = './', binsize = 100, promo_bed = Promoter_anno, fa ='/illumina/iGenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa')
+t2 <- Sys.time()
+t2 - t1
+write.table(bed_100bp_bg, 'hg19Bg.bed', sep = '\t', col.names = F, quote =F)
 
